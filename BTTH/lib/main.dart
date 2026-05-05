6095fbf24c275c0eeff1fdd8da4e27646f35d669
taplamcode <tranthanhngan.29.05.2005@gmail.com>
2026-05-05 15:55:24 +0700

    pull main

--- BTTH/lib/main.dart
+++ BTTH/lib/main.dart
@@ -7,11 +7,14 @@ import 'package:flutter/material.dart';
 import 'app/app.dart'; 
 import 'package:firebase_core/firebase_core.dart'; 
 import 'package:get/get.dart'; 
+import 'firebase_options.dart';
 import 'controller/login_controller.dart'; 
  
 void main() async { 
   WidgetsFlutterBinding.ensureInitialized(); 
-  await Firebase.initializeApp(); 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); 
  
   Get.put(AuthController()); 
   Get.put(NotificationController()); 
