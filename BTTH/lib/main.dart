785a24e7b3a0fdae433bd1361b7acc572b4afe81
LuuNghia <luudinhnghia30012005@gmail.com>
2026-05-05 14:36:32 +0700

    main

--- BTTH/lib/main.dart
+++ BTTH/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:draf_project/controller/cart_controller.dart';
 import 'package:draf_project/controller/notification_controller.dart';
 import 'package:draf_project/controller/order_controller.dart';
 import 'package:draf_project/controller/wishlist_controller.dart';
+import 'package:draf_project/controller/settings_controller.dart';
 import 'package:flutter/material.dart';
 import 'app/app.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -11,9 +12,10 @@ void main() async {
 WidgetsFlutterBinding.ensureInitialized();
 await Firebase.initializeApp();
 Get.put(AuthController());
+Get.put(NotificationController());
 Get.put(CartController());
 Get.put(OrderController());
 Get.put(WishlistController());
-Get.put(NotificationController());
+Get.put(SettingsController());
 runApp(MyApp());
 }
